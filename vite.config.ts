6082810b546e9944ca7189015ce502ca67e30bb8
varma519 <84116775+varma519@users.noreply.github.com>
2025-09-06 13:45:51 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',   // 👈 ensures assets resolve correctly with custom domain
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
