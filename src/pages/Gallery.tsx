b19c389d6ded166cbd206947fde0b36f1b21fec6
varma519 <84116775+varma519@users.noreply.github.com>
2025-09-06 12:07:26 -0500

    Updated Gallery.tsx

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -71,7 +71,7 @@ const Gallery = () => {
           </div>
           
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {galleryImages.slice(0, 7).map((image, index) => (
+            {galleryImages.slice(0, 8).map((image, index) => (
               <div
                 key={image.id}
                 className="group cursor-pointer relative overflow-hidden rounded-lg bg-gray-800 aspect-[3/4]"
